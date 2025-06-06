03592d4a1cb6923ab79915fa14f3f59aa68f26dd
JoJunHwa <jjj5306@naver.com>
2025-06-06 18:04:58 +0900

    fix : 환경 변수로 API 서버 주소를 가져오도록 설정

--- app/config.py
+++ app/config.py
@@ -36,17 +36,18 @@ class DevelopmentConfig(Config):
     SQLALCHEMY_ECHO = True  # SQL 쿼리 로깅
     
     # API 서버 설정 (Step2 결과 콜백용)
-    API_SERVER_URL = 'http://host.docker.internal:8080'
-    API_SERVER_CALLBACK_ENDPOINT = '/diagnosis/step2'
+    # .env 파일에서 가져오되, 없으면 개발환경 기본값 사용
+    API_SERVER_URL = os.environ.get('API_SERVER_URL') or 'http://host.docker.internal:8080'
+    API_SERVER_CALLBACK_ENDPOINT = os.environ.get('API_SERVER_CALLBACK_ENDPOINT') or '/diagnosis/step2'
 
 class ProductionConfig(Config):
     """프로덕션 환경 설정"""
     DEBUG = False
     SQLALCHEMY_ECHO = False
     
-    # API 서버 설정 (추후 설정)
-    API_SERVER_URL = os.environ.get('API_SERVER_URL')  # 환경변수에서만 가져옴
-    API_SERVER_CALLBACK_ENDPOINT = '/diagnosis/step2'
+    # API 서버 설정 (프로덕션에서는 반드시 .env에서 설정해야 함)
+    API_SERVER_URL = os.environ.get('API_SERVER_URL')  # 기본값 없음 - 반드시 설정 필요
+    API_SERVER_CALLBACK_ENDPOINT = os.environ.get('API_SERVER_CALLBACK_ENDPOINT') or '/diagnosis/step2'
 
 # Configuration dictionary
 config_by_name = {
